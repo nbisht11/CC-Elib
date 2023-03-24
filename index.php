7cd80b06fb1a8ded08d81ee68370c90a010c905f
Nitin Bisht <Nitin.Bisht@triconinfotech.com>
2023-03-24 11:48:09 +0530

    Removed DIV

--- index.php
+++ index.php
@@ -97,25 +97,6 @@ else
 $result = $conn->query($sql);
 ?>
 <body>
-<div class="container">
-  <div class=  "row justify-content-left" >
-    <?php
-    while($book=$result->fetch_assoc())
-    {?>
-      <div class="col-md-3 col-sm-12 cpd">
-        <div class="card h-100">
-            <img class="card-img-top" src="<?php echo $book['Book_Cover']?>" width=240px height="360px" >
-            <div class='card-body'>
-              <h4 class ='card-title' style="color:black;"><?php echo $book['Book_Name']?></h4>
-              <h5 class='card-text' style="color:black;"><?php echo $book['Author_Name']?></h5>
-              <a class="card-link" href="book_detail.php?id=<?php echo($book['Book_ID'])?>" target="_top"><p> Read More....</p></a>
-            </div>
-        </div>
-      </div>
-      <?php
-    }?>
-  </div>
-</div>
   <?php if(!isset($_GET['s']))
   {?>
   <nav aria-label="Page navigation" class="pgn">
